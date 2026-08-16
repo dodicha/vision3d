3efe6900a7b67ee73f1a3adf15c847b51d478c99
Giorgi gogichaishvili <g.dodicha@gmail.com>
2026-08-16 23:18:43 +0400

    action: animate the observer's object

--- src/app/components/World.tsx
+++ src/app/components/World.tsx
@@ -1,3 +1,84 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
+function Test1() {
+  useFrame(() => {
+    console.log("frame");
+  });
+
+  return null;
+}
+// console.log Works non-stop
+function Test2() {
+  useFrame((state, delta) => {
+    console.log("delta:", delta);
+  });
+}
+// delta is the time in seconds since the last frame. It is useful for creating smooth animations that are independent of the frame rate.
+function Test3() {
+  useFrame((state) => {
+    console.log("state:", state);
+  });
+  return null;
+}
+
+function TestCube() {
+  const meshRef = useRef<THREE.Mesh>(null);
+  {
+    /*
+  Observation:
+
+  useRef gives us a reference to the specific Mesh
+  we want to control.
+*/
+  }
+  useFrame((state) => {
+    const t = state.clock.elapsedTime;
+
+    meshRef.current!.position.x = Math.sin(t);
+    meshRef.current!.position.y = Math.sin(t);
+    meshRef.current!.position.z = Math.sin(t);
+    meshRef.current!.rotation.x = t;
+    meshRef.current!.rotation.y = Math.cos(t);
+    meshRef.current!.rotation.z = Math.sin(t);
+  });
+  {
+    /*
+  Observation:
+
+  useFrame runs on every rendered frame.
+  We can use the current time to change the object's state
+  from one frame to the next.
+*/
+  }
+  return (
+    <>
+      <mesh ref={meshRef} position={[1, 1, 0]} castShadow>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial color="red" />
+      </mesh>
+    </>
+  );
+}
+function TestCube1() {
+  const meshRef1 = useRef<THREE.Mesh>(null);
+
+  useFrame((state) => {
+    const t = state.clock.elapsedTime;
+
+    meshRef1.current!.rotation.x = t;
+    meshRef1.current!.rotation.y = Math.cos(t);
+    meshRef1.current!.rotation.z = Math.sin(t);
+  });
+  return (
+    <mesh ref={meshRef1} position={[5, 1, 0]} castShadow>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshBasicMaterial color="blue" />
+    </mesh>
+  );
+}
+
+// state contains information about the current frame, including the camera, scene, and renderer. It can be used to access and manipulate the 3D scene in real-time.
 export default function World() {
   {
     /*
@@ -13,7 +94,7 @@ export default function World() {
   }
   return (
     <>
-      <directionalLight position={[10, 50, 5]} intensity={4} castShadow />
+      <directionalLight position={[5, 15, 15]} intensity={4} castShadow />
       {/*
   Observation:
 
@@ -25,7 +106,11 @@ export default function World() {
   while changing its intensity changes the overall brightness
   without depending on the object's distance from the light.
 */}
-      <mesh position={[1, 2, 0]} castShadow>
+      <mesh position={[-5, 0, 0]} castShadow rotation={[0, 0, Math.PI / 4]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="green" />
+      </mesh>
+      <mesh position={[-2, 0, 0]} castShadow rotation={[0, 0, Math.PI / 4]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="green" />
       </mesh>
@@ -41,6 +126,11 @@ export default function World() {
 */}
         <meshStandardMaterial color="gray" />
       </mesh>
+      {/* <Test1 /> */}
+      {/* <Test2 /> */}
+      {/* <Test3 /> */}
+      <TestCube />
+      <TestCube1 />
     </>
   );
 }
